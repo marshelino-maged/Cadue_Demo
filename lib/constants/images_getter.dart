d468e374cb346046dfb7b92bc2bd895ef6c32259
marshelino-maged <maromaged169@gmail.com>
2024-08-01 05:16:25 +0200

    persistent Nav Bar Completed

--- lib/constants/images_getter.dart
+++ lib/constants/images_getter.dart
@@ -7,4 +7,10 @@ class ImagesGetter {
   static String get lock => '$_baseUrl/lock.png';
   static String get otp => '$_baseUrl/otp.png';
   static String get backButton => '$_baseUrl/Back.png';
+  static String get occasionIconNotActive => '$_baseUrl/occasion.png';
+  static String get accountIconNotActive => '$_baseUrl/account.png';
+  static String get occasionIconActive => '$_baseUrl/occasionActive.png';
+  static String get accountIconActive => '$_baseUrl/accountActive.png';
+  static String get notificationIcon => '$_baseUrl/notification.png';
+  static String get testIcon => '$_baseUrl/lovely.png';
 } 
---
+++ lib/presentation/screens/bottom_navbar_screen.dart
@@ -0,0 +1,48 @@
+import 'package:demo_project/constants/colors_getter.dart';
+import 'package:demo_project/constants/images_getter.dart';
+import 'package:demo_project/presentation/screens/account/account_screen.dart';
+import 'package:demo_project/presentation/screens/occasions/occasions_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+
+class BottomNavBarScreen extends StatelessWidget {
+  const BottomNavBarScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      context,
+      screens: [OccasionsScreen(), AccountScreen()],
+      items: [
+        PersistentBottomNavBarItem(
+          icon: Image.asset(ImagesGetter.occasionIconActive),
+          inactiveIcon: Image.asset(ImagesGetter.occasionIconNotActive),
+        ),
+        PersistentBottomNavBarItem(
+          icon: Image.asset(ImagesGetter.accountIconActive),
+          inactiveIcon: Image.asset(ImagesGetter.accountIconNotActive),
+        ),
+      ],
+      hideNavigationBarWhenKeyboardAppears: true,
+      popBehaviorOnSelectedNavBarItemPress: PopBehavior.all,
+      padding: const EdgeInsets.all(8),
+      backgroundColor: ColorsGetter.white255,
+      isVisible: true,
+      animationSettings: const NavBarAnimationSettings(
+        navBarItemAnimation: ItemAnimationSettings(
+          duration: Duration(milliseconds: 400),
+          curve: Curves.bounceIn,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimationSettings(
+          animateTabTransition: true,
+          duration: Duration(milliseconds: 200),
+          screenTransitionAnimationType: ScreenTransitionAnimationType.slide,
+        ),
+      ),
+      confineToSafeArea: true,
+      navBarHeight: kBottomNavigationBarHeight,
+      navBarStyle: NavBarStyle.simple,
+    );
+  }
+}
